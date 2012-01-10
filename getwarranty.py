ee1f8217551786c71d52b0e7890d5b03ddfa9266
Michael Lynn <pudquick@sogetthis.com>
2012-01-10 15:56:58 -0800

    Initial commit

---
+++ getwarranty.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys, requests, json
+
+standard_keys = (('PROD_DESCR', 'Product Description'),
+                 ('SERIAL_ID', 'Serial Number'),
+                 ('HW_COVERAGE_DESC', 'Warranty Type'),
+                 ('PURCHASE_DATE', 'Purchase Date'))
+
+asd_db = {}
+
+def init_asd_db():
+    if (not asd_db):
+        response = requests.get('https://raw.github.com/stefanschmidt/warranty/master/asdcheck')
+        for model,val in [model_str.strip().split(':') for model_str in response.content.split('\n') if model_str.strip()]:
+            asd_db[model] = val
+
+def warranty_json(sn, country='US'):
+    return json.loads(requests.get('https://selfsolve.apple.com/warrantyChecker.do', params={'country': country, 'sn': sn}).content[5:-1])
+
+def coverage_date(details):
+    coverage = 'EXPIRED'
+    if (details.has_key('COV_END_DATE') and (details['COV_END_DATE'] != u'')):
+        coverage = 'COV_END_DATE'
+    if (details.has_key('HW_END_DATE')):
+        coverage = 'HW_END_DATE'
+    return (coverage, 'Coverage')
+
+def asd_version(details):
+    init_asd_db()
+    return (asd_db.get(details['PROD_DESCR'], 'Not found')+"\n", 'ASD Version')
+
+def get_warranty(*serials):
+    for serial in serials:
+        info = warranty_json(serial)
+        if (info.has_key('ERROR_CODE')):
+            print "ERROR: Invalid key: %s\n" % (serial)
+        else:
+            for key,label in (standard_keys + (coverage_date(info), asd_version(info))):
+                print "%s: %s" % (label, info.get(key, key))
+
+def main():
+    for serial in sys.argv[1:]:
+        get_warranty(serial)
+        
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,31 @@
+#pyMacWarranty - Apple Warranty coverage checking in OS X using the built-in python 
+
+pyMacWarranty is a single python file script for checking warranty status on one or more Mac serial numbers, passed as arguments to the script. The only python requirement is the 'requests' module (http://docs.python-requests.org/en/latest/index.html).
+
+##Credits
+
+- pyMacWarranty is written by pudquick@github 
+
+##License
+
+pyMacWarranty is released under a standard MIT license.
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation files
+	(the "Software"), to deal in the Software without restriction,
+	including without limitation the rights to use, copy, modify, merge,
+	publish, distribute, sublicense, and/or sell copies of the Software,
+	and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+	BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+	ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
